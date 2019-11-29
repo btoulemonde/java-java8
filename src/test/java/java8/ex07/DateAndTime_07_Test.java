0fb7455bdb9ba2312b20f59a2e0b88b957bb789a
Baptiste TOULEMONDE <baptistetoul@gmail.com>
2019-11-29 15:09:13 +0100

    vrai fin tp datetime

--- src/test/java/java8/ex07/DateAndTime_07_Test.java
+++ src/test/java/java8/ex07/DateAndTime_07_Test.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 import java.time.DayOfWeek;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.ZoneId;
 import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -16,24 +18,27 @@ import static org.junit.Assert.assertThat;
  */
 public class DateAndTime_07_Test {
 
-    @Test
-    public void test_date_to_localdate() throws Exception {
+	@Test
+	public void test_date_to_localdate() throws Exception {
 
-        // TODO Créer une date Java 1 (12/02/2017)
-        Date date = new Date(117,1,12);
+		// TODO Créer une date Java 1 (12/02/2017)
+		Date date = new Date(117, 1, 12);
 
-        // TODO transformer la date en Instant
-        Instant instant = Instant.ofEpochSecond(3);
+		// TODO transformer la date en Instant
+		Instant instant = date.toInstant();
 
-        // TODO transformer la date en LocalDate
+		// TODO transformer la date en LocalDate
+		
+		ZoneId currentZone = ZoneId.systemDefault();
+		LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, currentZone);
+		LocalDate result =localDateTime.toLocalDate() ;
+		
+		// TODO valoriser les différentes variables afin de rendre le test
+		// passant
 
-        LocalDate result = LocalDate.of(2017, 02, 12);
+		assertThat(result.getYear(), is(2017));
+		assertThat(result.getMonth(), is(Month.FEBRUARY));
+		assertThat(result.getDayOfMonth(), is(12));
 
-        // TODO valoriser les différentes variables afin de rendre le test passant
-
-        assertThat(result.getYear(), is(2017));
-        assertThat(result.getMonth(), is(Month.FEBRUARY));
-        assertThat(result.getDayOfMonth(), is(12));
-
-    }
+	}
 }
